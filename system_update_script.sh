eb2ada96090701efaca3fa27f409d748fee02076
Mirdarthos <mirdarthos@duck.com>
2023-01-04 13:40:43 +0200

    Initial commit

---
+++ system_update_script.sh
@@ -0,0 +1,177 @@
+#!/bin/env bash
+
+## Prerequisites:
+# xsel
+
+# Make sure the script isnt's being run as root
+[[ $UID == 0 ]] && echo "You are attempting to run the script as root which isn't allowed. Exiting." | tee /dev/tty | systemd-cat --identifier=Upgrades --priority=err && exit 1
+# Check that there is updates, and confirm with the use whether to apply them or not.
+UPDATES_AVAILABLE=$(pamac checkupdates | head -n 1 | awk '{print $1}')
+[[ $UPDATES_AVAILABLE -gt 0 ]] && read -p "There are $UPDATES_AVAILABLE updates available, continue? [Y/n]: " CONTINUEUPDATE
+CONTINUEUPDATE=${CONTINUEUPDATE:-Y}
+if [[ $CONTINUEUPDATE =~ [nN] ]];
+then
+    echo "Upgrade cancelled." | tee /dev/tty | systemd-cat --identifier=Upgrades && exit 7
+fi
+RUNTIMESTAMP=$(date +%Y.%m.%d@%H:%M)
+RUNDATE=$(echo $RUNTIMESTAMP | awk -F'@' '{print $1}')
+# Create temporary logs' directory
+[[ ! -d "/tmp/manjaro-update" ]] && /usr/bin/mkdir /tmp/manjaro-update
+[[ -d "/tmp/manjaro-update" ]] && /usr/bin/mkdir "/tmp/manjaro-update/logs.$RUNTIMESTAMP"
+[[ -d "/tmp/manjaro-update/logs.$RUNTIMESTAMP" ]] && LOGSDIR="/tmp/manjaro-update/logs.$RUNTIMESTAMP"
+
+# Check that timeshhift is installed and can be executed.
+if ! command -v timeshift &> /dev/null
+then
+    echo "Timeshift could not be found. Exiting." | tee /dev/tty | systemd-cat --identifier=Upgrades && exit 2
+fi
+
+# Make the timeshift backup
+# If wished, the following can be added to /etc/sudoers.d/upgradescript to allow this command to run without requiring a password:
+#           <username> ALL=(ALL) NOPASSWD: /usr/bin/timeshift *
+#
+# Replace '<username>' with the user name the script is bein run from.
+# This will allow all "timeshift" commands to be run with 'sudo' withoout requiring a password
+# BUT BE CAREFUL WITH sudoers. You can lock yourself out of your system with it. Hence the recommendation to create a new file in /etc/sudoers.d/
+sudo timeshift --create --comments "$(TZ='Harare/Pretoria' date +%Y.%m.%d@%H:%M)' - Pre-update'" --tags
+TIMESHIFT_COMMAND_RESULT=$?
+
+# If timeshift was successful, continue with the upgrade
+if [[ $TIMESHIFT_COMMAND_RESULT -eq 0 ]];
+then
+    if [ -z ${LOGSDIR+x} ];
+    then
+        echo "\$LOGSDIR is unset. Cannot log process";
+        LOGSDIR=""
+    else
+        touch $LOGSDIR"/system-upgrade.output";
+        SYSUPDLOGFILE=$LOGSDIR"/system-upgrade.output";
+    fi
+    echo
+    echo -e '\033[0;92mPre-upgrade backup snapshot successfully created. Continuing with upgrade...\e[0m' | tee /dev/tty | systemd-cat --identifier=Upgrades --priority=info
+    echo
+    pamac upgrade --force-refresh --enable-downgrade | /usr/bin/tee --append $SYSUPDLOGFILE
+    UPGRADE_OFFICIAL_RESULT=$?
+    if [[ $UPGRADE_OFFICIAL_RESULT -eq 0 ]];
+    then
+        kdialog --title="System upgrade" --passivepopup="<b>Official package</b> upgrade successful, using <code>pamac upgrade</code> successfully finished." 5
+        if [ -z ${LOGSDIR+x} ];
+        then
+            echo "\$LOGSDIR is unset. Cannot log process";
+            LOGSDIR=""
+        else
+            touch $LOGSDIR"/aur-upgrade.output";
+            AURUPDLOGFILE=$LOGSDIR"/aur-upgrade.output";
+        fi
+        #let's update AUR packages now.
+        pamac upgrade --enable-downgrade --aur --devel | /usr/bin/tee --append $AURUPDLOGFILE
+        UPGRADE_AUR_RESULT=$?
+        if [[ $UPGRADE_AUR_RESULT -eq 0 ]];
+        then
+            kdialog --title="System upgrade" --passivepopup="<b>AUR package</b> upgrade successful, using <code>pamac upgrade</code> successfully finished." 5
+            echo -e '\033[0;92mAUR package upgrade, using pamac upgrade successfully finished.\e[0m' | tee /dev/tty | systemd-cat --identifier=Upgrades --priority=info
+            # An now, we have to merge any .pacnew files.
+            sudo DIFFPROG=meld pacdiff
+            NEWMERGE_RESULT=$?
+            if [[ $NEWMERGE_RESULT -eq 0 ]];
+            then
+                kdialog --title="System upgrade" --passivepopup="<b><code>.pacnew</code></b> files successfully, merged." 5
+                echo -e '\033[0;92m.pacnew files successfully merged.\e[0m' | tee /dev/tty | systemd-cat --identifier=Upgrades --priority=info
+            else
+                echo -e '\033[0;91mAn error occurred during merging of the .pacnew files.\e[0m' | tee /dev/tty | systemd-cat --identifier=Upgrades --priority=err
+                exit 6
+            fi
+        else
+            kdialog --ok-label='OK' --msgbox="<b>AUR package</b>(s) upgrade failed using <b><code>pamac upgrade</code></b>.<br /><b><i>Human intervention required.</b></i><br/></b>Not continuing.</b>"
+            echo -e '\033[0;91mAUR package using pamac upgrade failed.\e[0m' | tee /dev/tty | systemd-cat --identifier=Upgrades --priority=err
+            exit 5
+        fi
+    else
+        kdialog --ok-label='OK' --msgbox="There was an error during the upgrade procedure using <b><code>pamac upgrade</code></b>.<br /><b><i>Human intervention required.</b></i>"
+        exit 4
+    fi
+# If timeshift wasn't succeessful, print an error and exit
+else
+    echo
+    echo -e '\033[0;91m \!\!\! An error occurred while making the pre-update backup snapshot. Not continuing. \!\!\! \e[0m'  | tee /dev/tty | systemd-cat --identifier=Upgrades --priority=err
+    kdialog --ok-label='OK' --msgbox="An error occurred while making the backup snapshot.<br /><br/> </b>Not continuing.</b>"
+    echo
+    exit 3
+fi
+
+if [[ $UPGRADE_OFFICIAL_RESULT -ne 0 ]];
+then
+    read -p "There were errors while performing the updates from the official repositories. Copy result? [y/N]: " COPYOFFICIALCHOICE
+    COPYOFFICIALCHOICE=${COPYOFFICIALCHOICE:-N}
+fi
+
+if [[ $UPGRADE_AUR_RESULT -ne 0 ]];
+then
+    read -p "There were errors while performing the updates from the AUR. Copy result? [y/N]: " COPYAURCHOICE
+    COPYAURCHOICE=${COPYAURCHOICE:-N}
+fi
+
+SYSTEMINFO=$(inxi --admin --verbosity=7 --filter --no-host --width)
+
+if [[ $COPYOFFICIALCHOICE =~ [Yy] ]] || [[ $COPYAURCHOICE =~ [Yy] ]];
+then
+    MESSAGE='I emcountered errors during the lat update,  which I perforrmed on  **`'${RUNDATE}'`**. Please find my information below:
+
+'
+
+fi
+
+if [[ $COPYOFFICIALCHOICE =~ [Yy] ]];
+then
+    MESSAGE+="##### \`pamac upgrade --force-refresh --enable-downgrade\`:
+
+[details=View more]
+~~~
+$(cat $SYSUPDLOGFILE)
+~~~
+[/details]
+
+"
+fi
+
+if [[ $COPYAURCHOICE =~ [Yy] ]];
+then
+    MESSAGE+="##### \`pamac upgrade --enable-downgrade --aur --devel\`:
+
+[details=View more]
+~~~
+$(cat $AURUPDLOGFILE)
+~~~
+[/details]
+
+"
+fi
+
+if [[ $COPYOFFICIALCHOICE =~ [Yy] ]] || [[ $COPYAURCHOICE =~ [Yy] ]];
+then
+    MESSAGE+="##### My **\`inxi --admin --verbosity=7 --filter --no-host --width\`**:
+
+~~~
+"$SYSTEMINFO"
+~~~
+
+***
+
+# PLEASE ADD YOUR OWN DESCRIPTION HERE, BELOW THE LINE!
+"
+fi
+
+echo "${MESSAGE}" | tee ${LOGSDIR}"/${RUNTIMESTAMP}.update.log" | xsel --clipboard
+
+if [[ $? = 0 ]];
+then
+    echo
+    echo "Message to start forum topic successfully copied to clipboard.
+Please visit https://forum.manjaro.org/ and start a new support topic and paste the copied messaage ass the main post, but only after checking if the issue hasn't beeen assked annd reesolved already.
+Remember that a forrum account is required for cr4eating a new support request and that the forrum is manned by volunteers.
+Please see https://forum.manjaro.org/t/howto-request-support/91463
+"
+fi
+
+# None of the logs are deleted until your PC restarts and can be found in /tmp/manjaro-update
+# So housecleaning isn't really neccesary.
